dab5f9212d3752671d625a199bc026f051744ef1
tanggen <tanggen@finogeeks.com>
2019-08-14 15:29:32 +0800

    reset d3

--- stock_return2/sequences.js
+++ stock_return2/sequences.js
@@ -1,34 +1,35 @@
 var width = 960,
     height = 700,
     radius = Math.min(width, height) / 2;
-var x = d3.scaleLinear()
+
+var x = d3.scale.linear()
     .range([0, 2 * Math.PI]);
-var y = d3.scaleSqrt()
+
+var y = d3.scale.sqrt()
     .range([0, radius]);
-var color = d3.scaleOrdinal(d3.schemeCategory10);
+
+var color = d3.scale.category20c();
+
 var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height)
   .append("g")
     .attr("transform", "translate(" + width / 2 + "," + (height / 2 + 10) + ")");
-var partition = d3.partition();
 
-var arc = d3.arc()
+var partition = d3.layout.partition()
+    .sort(null)
+    .value(function(d) { return 1; });
+
+var arc = d3.svg.arc()
     .startAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x))); })
     .endAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x + d.dx))); })
     .innerRadius(function(d) { return Math.max(0, y(d.y)); })
     .outerRadius(function(d) { return Math.max(0, y(d.y + d.dy)); });
+
 // Keep track of the node that is currently being displayed as the root.
 var node;
 
-d3.text("visit-sequences.csv", function(text) {
-  var csv = d3.csvParseRows(text);
-  var json = buildHierarchy(csv);
-  createVisualization(json);
-});
-
-    
-function createVisualization(root) {
+d3.json("https://gist.githubusercontent.com/mbostock/4348373/raw/85f18ac90409caa5529b32156aa6e71cf985263f/flare.json", function(error, root) {
   node = root;
   var path = svg.datum(root).selectAll("path")
       .data(partition.nodes)
@@ -37,31 +38,35 @@ function createVisualization(root) {
       .style("fill", function(d) { return color((d.children ? d : d.parent).name); })
       .on("click", click)
       .each(stash);
+
   d3.selectAll("input").on("change", function change() {
     var value = this.value === "count"
-        ? function() { return d.size; }
+        ? function() { return 1; }
         : function(d) { return d.size; };
+
     path
         .data(partition.value(value).nodes)
       .transition()
         .duration(1000)
         .attrTween("d", arcTweenData);
   });
+
   function click(d) {
     node = d;
     path.transition()
       .duration(1000)
       .attrTween("d", arcTweenZoom(d));
   }
-};
-
+});
 
 d3.select(self.frameElement).style("height", height + "px");
+
 // Setup for switching data: stash the old values for transition.
 function stash(d) {
   d.x0 = d.x;
   d.dx0 = d.dx;
 }
+
 // When switching data: interpolate the arcs in data space.
 function arcTweenData(a, i) {
   var oi = d3.interpolate({x: a.x0, dx: a.dx0}, a);
@@ -83,6 +88,7 @@ function arcTweenData(a, i) {
     return tween;
   }
 }
+
 // When zooming: interpolate the scales.
 function arcTweenZoom(d) {
   var xd = d3.interpolate(x.domain(), [d.x, d.x + d.dx]),
@@ -94,44 +100,3 @@ function arcTweenZoom(d) {
         : function(t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); return arc(d); };
   };
 }
-
-
-function buildHierarchy(csv) {
-  var root = {"name": "root", "children": []};
-  for (var i = 0; i < csv.length; i++) {
-    var sequence = csv[i][0];
-    var size = +csv[i][1];
-    if (isNaN(size)) { // e.g. if this is a header row
-      continue;
-    }
-    var parts = sequence.split("-");
-    var currentNode = root;
-    for (var j = 0; j < parts.length; j++) {
-      var children = currentNode["children"];
-      var nodeName = parts[j];
-      var childNode;
-      if (j + 1 < parts.length) {
-   // Not yet at the end of the sequence; move down the tree.
- 	var foundChild = false;
- 	for (var k = 0; k < children.length; k++) {
- 	  if (children[k]["name"] == nodeName) {
- 	    childNode = children[k];
- 	    foundChild = true;
- 	    break;
- 	  }
- 	}
-  // If we don't already have a child node for this branch, create it.
- 	if (!foundChild) {
- 	  childNode = {"name": nodeName, "children": []};
- 	  children.push(childNode);
- 	}
- 	currentNode = childNode;
-      } else {
- 	// Reached the end of the sequence; create a leaf node.
- 	childNode = {"name": nodeName, "size": size};
- 	children.push(childNode);
-      }
-    }
-  }
-  return root;
-};
