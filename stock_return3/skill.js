747fa89733637a4f984844e7e532417c3c294440
tanggen <tanggen@finogeeks.com>
2019-08-28 13:22:44 +0800

    update v3

--- stock_return3/skill.js
+++ stock_return3/skill.js
@@ -157,8 +157,8 @@ var chart = function (d3) {
         g = 500 - rect.left - rect.right,
         h = 400 - rect.top - rect.bottom,
         i = [1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013],
-        j = d3.scaleLinear().range([0, g]),
-        k = d3.scaleLinear().range([h, 0]),
+        j = d3.scale.linear().range([0, g]),
+        k = d3.scale.linear().range([h, 0]),
         bottomtick = d3
             .svg
             .axis()
